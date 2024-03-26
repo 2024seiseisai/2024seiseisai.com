ece6871b3819be02a14a5c874305fd53f3cc1981
Reito-74 <reito1484reito@gmail.com>
2024-03-26 18:21:28 +0900

    test:メニュー表示・非表示の調整

--- webサイトtest/move.js
+++ webサイトtest/move.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     //メニューーの開閉を示す変数を定義
     let menu_open_rate = 0
+    //スクロール量取得用の変数を定義
+    let scroll_now = 0;
+    let scroll_pre = 0;
 
 
     function changeContent(object,original_text,changed_text){
@@ -30,6 +33,23 @@ $(document).ready(function(){
             $("body").css("overflow","visible");
             $(".menu_large").css("transform","scaleY(0)")
             menu_open_rate = 0;
+        }
+    });
+
+
+    //相対的なスクロール量を取得する関数
+    function get_scroll(){
+        scroll_pre = scroll_now;
+        scroll_now = $(window).scrollTop();
+        return (scroll_now - scroll_pre);
+    };
+
+    //スクロール時にメニューを表示・非表示にする関数
+    $(window).on('scroll',function(){
+        if(get_scroll() >= 0){
+            $(".menu").css("display","none");
+        }else{
+            $(".menu").css("display","flex");
         }
     });
 });
