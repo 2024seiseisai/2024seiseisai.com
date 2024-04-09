92a9ee5f153c1fbce6df4350c24bd732ca8a8323
Reito-74 <reito1484reito@gmail.com>
2024-04-09 21:17:33 +0900

    メニュー追加

--- 2024/script.js
+++ 2024/script.js
@@ -1,4 +1,7 @@
-let menu = "closed"
+//メニューの開閉状況を示す変数を定義
+let menu = "closed";
+
+
 
 //ハンバーガーのアニメーション
 var hamburger_animation_PC = lottie.loadAnimation({
@@ -8,17 +11,26 @@ var hamburger_animation_PC = lottie.loadAnimation({
     autoplay: false,
     path: "top_img/hamburger.json" // JSONファイルのパス
 });
-hamburger_animation_PC.setSpeed(2.0);
+hamburger_animation_PC.setSpeed(4.0);
 
+
+
+//メニューの開閉
 $("#hamburger").click(function(){
     if(menu === "closed"){
         hamburger_animation_PC.setDirection(1);
-        hamburger_animation_PC.play()
-        menu = "opened"
+        hamburger_animation_PC.play();
+        $("body").css("overflow","hidden");
+        $(".menu").css("transform","scaleY(1)");
+        $("header").css("border-bottom","1px solid #FFFFFF");
+        menu = "opened";
     }else if(menu === "opened"){
         hamburger_animation_PC.setDirection(-1); // 逆再生方向を設定
         hamburger_animation_PC.play(); // アニメーションを再生
-        menu = "closed"
+        $("body").css("overflow","visible");
+        $(".menu").css("transform","scaleY(0)");
+        $("header").css("border-bottom","");
+        menu = "closed";
     }
 });
     
