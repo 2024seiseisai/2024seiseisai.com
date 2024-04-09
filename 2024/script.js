53436a2ad4d8da4e5d638eee137426656c35be56
Reito-74 <reito1484reito@gmail.com>
2024-04-09 21:51:54 +0900

    メニューのアニメーションを調整

--- 2024/script.js
+++ 2024/script.js
@@ -11,7 +11,7 @@ var hamburger_animation_PC = lottie.loadAnimation({
     autoplay: false,
     path: "top_img/hamburger.json" // JSONファイルのパス
 });
-hamburger_animation_PC.setSpeed(4.0);
+hamburger_animation_PC.setSpeed(2.0);
 
 
 
@@ -20,14 +20,12 @@ $("#hamburger").click(function(){
     if(menu === "closed"){
         hamburger_animation_PC.setDirection(1);
         hamburger_animation_PC.play();
-        $("body").css("overflow","hidden");
         $(".menu").css("transform","scaleY(1)");
         $("header").css("border-bottom","1px solid #FFFFFF");
         menu = "opened";
     }else if(menu === "opened"){
         hamburger_animation_PC.setDirection(-1); // 逆再生方向を設定
         hamburger_animation_PC.play(); // アニメーションを再生
-        $("body").css("overflow","visible");
         $(".menu").css("transform","scaleY(0)");
         $("header").css("border-bottom","");
         menu = "closed";
