d516fd06897105f96d673a3caeb43f1006c173e4
Reito-74 <reito1484reito@gmail.com>
2024-04-09 18:29:17 +0900

    ハンバーガーにアニメーショントリガーを追加

--- 2024/script.js
+++ 2024/script.js
@@ -1,8 +1,12 @@
 //ハンバーガーのアニメーション
-lottie.loadAnimation({
+var hamburger_animation_PC = lottie.loadAnimation({
     container: document.getElementById('hamburger'),// アニメーションを格納するDOM要素 
     renderer: 'svg',
     loop: false,
     autoplay: false,
     path: "top_img/hamburger.json" // JSONファイルのパス
+});
+
+$("#hamburger").click(function(){
+    hamburger_animation_PC.playSegments([0, 100], true);
 });
