611f85d6ed0fc3fad32cf5a8472cb18c046f0bea
Reito-74 <reito1484reito@gmail.com>
2024-04-10 14:47:10 +0900

    メニューの挙動変更、メニューにSNS追加

--- 2024/script.js
+++ 2024/script.js
@@ -20,16 +20,18 @@ $("#hamburger").click(function(){
     if(menu === "closed"){
         hamburger_animation_PC.setDirection(1);
         hamburger_animation_PC.play();
-        $(".menu").css("transform","scaleY(1)");
+        $(".menu").css("top","100px");
         $("header").css("border-bottom","1px solid #FFFFFF");
         menu = "opened";
     }else if(menu === "opened"){
         hamburger_animation_PC.setDirection(-1); // 逆再生方向を設定
         hamburger_animation_PC.play(); // アニメーションを再生
-        $(".menu").css("transform","scaleY(0)");
+        $(".menu").css("top","-100vh");
         menu = "closed";
         setTimeout(function(){
-            $("header").css("border-bottom","");
+            if(menu === "closed"){
+                $("header").css("border-bottom","");
+            }
         },200);
     }
 });
