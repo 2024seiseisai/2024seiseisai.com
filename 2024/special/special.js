5392a75d0f91e35304f68e4f7dc38583ddf90d72
masawin10 <masahirowindows@yahoo.co.jp>
2024-08-02 12:38:44 +0900

    splide部分 去年からほぼOEM 矢印なし非アクティブ部希薄化なしXHEADER一個ずつ

---
+++ 2024/special/special.js
@@ -0,0 +1,102 @@
+new Splide( '.wallpapers', {
+    type   : 'loop',
+    perPage: 3,
+    perMove: 1,
+    gap: "8%",
+    padding: "18%",
+    focus  : 'center',
+
+    flickPower: 300,
+
+    autoplay: true,
+    interval: 5000,
+
+    arrows: true,
+
+    classes: {
+        arrow : 'splide__arrow splide_arrow',
+		prev  : 'splide__arrow--prev arrow--prev',
+		next  : 'splide__arrow--next arrow--next',
+        pagination: 'splide__pagination paginations',
+        page  : 'splide__pagination__page pagination',
+    },
+
+    breakpoints: {
+		1025: {
+            perPage: 1,
+            padding: "30%",
+            gap: 35,
+
+            arrows: false,
+		},
+    }
+}).mount();
+
+new Splide( '.icons', {
+    type   : 'loop',
+    perPage: 3,
+    perMove: 1,
+    gap: "10%",
+    padding: "18%",
+    focus  : 'center',
+
+    flickPower: 300,
+
+    autoplay: true,
+    interval: 5000,
+
+    arrows: true,
+
+    classes: {
+        arrow : 'splide__arrow splide_arrow',
+		prev  : 'splide__arrow--prev arrow--prev',
+		next  : 'splide__arrow--next arrow--next',
+        pagination: 'splide__pagination paginations',
+        page  : 'splide__pagination__page pagination',
+    },
+
+    breakpoints: {
+		1025: {
+            perPage: 1,
+            padding: "30%",
+            gap: 35,
+
+            arrows: false,
+		},
+    }
+}).mount();
+
+new Splide( '.headers', {
+
+    type   : 'loop',
+    perPage: 3,
+    perMove: 1,
+    gap: "65%",
+    padding: "30%",
+    focus  : 'center',
+
+    flickPower: 300,
+
+    autoplay: true,
+    interval: 5000,
+
+    arrows: true,
+
+    classes: {
+        arrow : 'splide__arrow splide_arrow',
+		prev  : 'splide__arrow--prev arrow--prev',
+		next  : 'splide__arrow--next arrow--next',
+        pagination: 'splide__pagination paginations',
+        page  : 'splide__pagination__page pagination',
+    },
+
+    breakpoints: {
+		1025: {
+            perPage: 1,
+            padding: "30%",
+            gap: "80%",
+
+            arrows: false
+		},
+    }
+}).mount();
