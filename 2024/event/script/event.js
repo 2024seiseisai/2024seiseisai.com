01ae0efe7f215185ba80fa2c9f6699d29aeb45bb
sortA0329 <tsouta0329@gmail.com>
2024-08-28 17:00:14 +0900

    だいたい完成

--- 2024/event/script/event.js
+++ 2024/event/script/event.js
@@ -1,89 +1,6 @@
-function GetLocationInfo(arr, day) {
-    if (arr.length == 0) return "";
-    let res = `<div class="location"><img src="/2024/event/img/map_pin.svg"><p>【${day}日目】</p>`;
-    arr.forEach((val) => {
-        res += `<p>　${val.location}　${val.start_h}:${("00" + val.start_m).slice(-2)}-${val.end_h}:${("00" + val.end_m).slice(-2)}　${val.summary === undefined ? "" : val.summary}</p>`;
-    });
-    res += "</div>";
-    return res;
-}
-$(document).ready(async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     let json = await fetch("./info/events.json");
     events = JSON.parse(await json.text());
-
-    if (events.is_rainy.day1 != "none" || events.is_rainy.day2 != "none") {
-        let events_list = document.getElementById("events_list");
-        events_list.insertAdjacentHTML(
-            "beforebegin",
-            `
-<div id="announce_rain">
-    <p id="announce1">
-        ${events.is_rainy.day1 == "all" && events.is_rainy.day2 == "none" ? "1日目(9/7)はすべて雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "partial" && events.is_rainy.day2 == "none" ? "1日目(9/7)は一部雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "none" && events.is_rainy.day2 == "all" ? "2日目(9/8)はすべて雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "none" && events.is_rainy.day2 == "partial" ? "2日目(9/8)は一部雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "all" && events.is_rainy.day2 == "all" ? "今年は両日ともにすべて雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "all" && events.is_rainy.day2 == "partial" ? "今年は一部雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "partial" && events.is_rainy.day2 == "all" ? "今年は一部雨天スケジュールでの開催となりました。" : ""}
-        ${events.is_rainy.day1 == "partial" && events.is_rainy.day2 == "partical" ? "今年は両日ともに一部雨天スケジュールでの開催となりました。" : ""}</p>
-    <p id="announce2">雨天スケジュールに変更されたため、このページに記載の時間は正しくないことがあります。詳しくは雨天スケジュールをご確認ください。</p>
-</div>
-`
-        );
-    }
-
-    {
-        let events_list = document.getElementById("events_list");
-        let cnt = 0;
-        events.events.forEach((event) => {
-            cnt += 1;
-            events_list.insertAdjacentHTML(
-                "beforeend",
-                `
-<div class="events_acc">
-    <div class="acc_button">
-        <img src="/2024/event/img/acc_play.svg" class="acc_button_img">
-        <p class="acc_title">${event.name}</p>
-        <div class="ticket_space">
-            <div class="ticket_box">
-                ${event.ticket === true ? '<img src="/2024/event/img/ticket_box.svg"><p>要整理券</p>' : ""}
-            </div>
-        </div>
-    </div>
-    <div class="acc_box">
-        <div class="box_space"></div>
-        ${GetLocationInfo(event.day1, 1)}
-        ${GetLocationInfo(event.day2, 2)}
-        <p class="event_summary">${event.summary}</p>
-    </div>
-</div>
-`
-            );
-        });
-    }
-
-    {
-        let acc = document.querySelectorAll(".acc_button");
-        acc.forEach((element) => {
-            element.addEventListener("click", () => {
-                acc.forEach((element2) => {
-                    if (element === element2) return;
-                    let accbox = element2.nextElementSibling;
-                    if (element2.classList.contains("acc_open")) {
-                        accbox.style.height = "0";
-                        element2.classList.remove("acc_open");
-                    }
-                });
-                let accbox = element.nextElementSibling;
-                if (element.classList.contains("acc_open")) {
-                    accbox.style.height = "0";
-                } else {
-                    accbox.style.height = accbox.scrollHeight + "px";
-                }
-                element.classList.toggle("acc_open");
-            });
-        });
-    }
 
     {
         let elements = [document.getElementById("day1_button"), document.getElementById("day2_button")];
@@ -113,10 +30,12 @@ $(document).ready(async () => {
         events.events.forEach((event) => {
             event.day1.forEach((val) => {
                 if (!(val.location in ev_lists)) return;
+                val.event_name = event.name;
                 ev_lists[val.location].day1.push(val);
             });
             event.day2.forEach((val) => {
                 if (!(val.location in ev_lists)) return;
+                val.event_name = event.name;
                 ev_lists[val.location].day2.push(val);
             });
         });
@@ -155,7 +74,7 @@ ${ev_lists[val.name].day1
 <div class="table_element day1" style="--start_h: ${ev.start_h}; --start_m: ${ev.start_m}; --end_h: ${ev.end_h}; --end_m: ${ev.end_m}; --color: ${ev_lists[val.name].color}">
     <div class="table_element_content">
         <p>${ev.name}</p>
-        <img src="/2024/event/img/arrow_circle.svg">
+        <img src="/2024/event/img/arrow_circle.svg" class="${ev.event_name}">
     </div>
     <div class="table_element_bar"></div>
     <div class="table_element_back" style="--padding_top: ${ev_lists[val.name].day1_bound2.includes(ev.start_h * 60 + ev.start_m) ? "2px" : "0"}; --padding_bottom: ${ev_lists[val.name].day1_bound2.includes(ev.end_h * 60 + ev.end_m) ? "2px" : "0"}"></div>
@@ -185,7 +104,7 @@ ${ev_lists[val.name].day2
         document.querySelectorAll(".table_element_content img").forEach((el) =>
             el.addEventListener("click", () => {
                 if (!document.getElementById(el.parentElement.parentElement.classList.contains("day1") ? "day1_button" : "day2_button").classList.contains("day_selected")) return;
-                document.querySelector("#main_container").style.transform = "translateX(-50%)";
+                window.location.href = "./event-list.html#" + el.classList[0];
             })
         );
     }
@@ -220,6 +139,3 @@ ${ev_lists[val.name].day2
         });
     }
 });
-function PushReturnButton() {
-    document.querySelector("#main_container").style.transform = "translateX(0%)";
-}
